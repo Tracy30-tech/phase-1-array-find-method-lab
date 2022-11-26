e5270d0593481685d7e92aee09846d39042d8270
Tracy <tracy.atieno@student.moringaschool.com>
2022-11-26 22:31:29 +0300

    array-find-method

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-// code your solution here
+function superbowlWin(record) {
+    let race = record.find((race)=>{
+        if (race.result==='W'){
+            return true;
+        }
+    })
+        if (race){
+            return race.year
+        }
+}
